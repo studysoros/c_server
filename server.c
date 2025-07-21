e3aa7ab00623f407db9db312108735941f5adb43
fatsummercat <homelnw77@gmail.com>
2025-07-21 10:04:16 +0700

    declare and populate server addr

--- server.c
+++ server.c
@@ -1,5 +1,7 @@
 #include <sys/socket.h>
 #include <stdio.h>
+#include <arpa/inet.h>
+#include <string.h>
 
 int main(void) {
     int tcp_socket = socket(AF_INET, SOCK_STREAM, 0);
@@ -9,4 +11,10 @@ int main(void) {
         return 1;
     }
     printf("socket created\n");
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(8080);
+    server_addr.sin_addr.s_addr = INADDR_ANY;
 }
